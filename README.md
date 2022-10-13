5372024bb4e683782b9551f1745d4456f96255a4
julianodorsti <julianodossti@gmail.com>
2022-10-12 21:47:35 -0300

    Alteracao script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # linux-vagrant
+# Testando o github 
+
--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install -y nginx && apt install -y vim && apt install -y curl && apt install -y telnet && apt install -y unzip && apt install -y wget && apt install -y net-tools && apt install -y htop && apt install -y nmap
+apt install -y vim curl telnet unzip wget net-tools htop nmap
 
 sysctl -w kernel.hostname=Devops22
 
